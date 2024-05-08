e995a03fbfe370337f8c9832a64f14f433829091
miyu suzuki <bu.m6s.pq@gmail.com>
2024-05-08 11:13:28 +0900

    会員登録後のリダイレク先修正

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,8 +41,6 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
-        return redirect(route('top', absolute: false));
+        return redirect(route('login', absolute: false));
     }
 }
