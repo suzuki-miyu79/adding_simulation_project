6d72a7978980b8a382ebe0b8c25f0873ae0889fb
miyu suzuki <bu.m6s.pq@gmail.com>
2024-05-12 21:10:15 +0900

    ページネーション修正

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -8,33 +8,56 @@
                 </li>
             @else
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev"
+                        aria-label="@lang('pagination.previous')">&lsaquo;</a>
                 </li>
             @endif
 
             {{-- Pagination Elements --}}
-            @foreach ($elements as $element)
-                {{-- "Three Dots" Separator --}}
-                @if (is_string($element))
-                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
-                @endif
+            {{-- Always show first page --}}
+            <li class="page-item"><a class="page-link" href="{{ $paginator->url(1) }}">1</a></li>
 
-                {{-- Array Of Links --}}
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
-                        @else
-                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
-                        @endif
-                    @endforeach
+            {{-- Show ellipsis if current page - 2 is greater than or equal to 4 --}}
+            @if ($paginator->currentPage() - 2 >= 4)
+                <li class="page-item disabled" aria-disabled="true"><span class="page-link">...</span></li>
+            @endif
+
+            {{-- Show page numbers within current page - 2 to current page + 2 range --}}
+            @for ($i = max(2, $paginator->currentPage() - 2); $i <= min($paginator->lastPage(), $paginator->currentPage() + 2); $i++)
+                @if ($i !== 1 && $i !== $paginator->lastPage())
+                    {{-- Avoid repeating first and last page --}}
+                    <li class="page-item @if ($i === $paginator->currentPage()) active @endif" aria-current="page">
+                        <a class="page-link" href="{{ $paginator->url($i) }}">{{ $i }}</a>
+                    </li>
                 @endif
-            @endforeach
+            @endfor
+
+            {{-- Show page 2 if current page - 2 is 3 --}}
+            @if ($paginator->currentPage() - 2 === 3)
+                <li class="page-item"><a class="page-link" href="{{ $paginator->url(2) }}">2</a></li>
+            @endif
+
+            {{-- Show ellipsis if current page + 2 is less than or equal to last page - 3 --}}
+            @if ($paginator->currentPage() + 2 < $paginator->lastPage() - 3)
+                <li class="page-item disabled" aria-disabled="true"><span class="page-link">...</span></li>
+            @endif
+
+            {{-- Show last page if current page + 2 is last page - 2 --}}
+            @if ($paginator->currentPage() + 2 === $paginator->lastPage() - 2)
+                <li class="page-item"><a class="page-link"
+                        href="{{ $paginator->url($paginator->lastPage() - 1) }}">{{ $paginator->lastPage() - 1 }}</a>
+                </li>
+            @endif
+
+            {{-- Always show last page --}}
+            <li class="page-item"><a class="page-link"
+                    href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a></li>
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next"
+                        aria-label="@lang('pagination.next')">&rsaquo;</a>
                 </li>
             @else
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
