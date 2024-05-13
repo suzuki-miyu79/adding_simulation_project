d8a00e44792155b5beb8fc633fdcf17468ee429b
miyu suzuki <bu.m6s.pq@gmail.com>
2024-05-13 09:10:50 +0900

    ページネーション修正

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -13,45 +13,36 @@
                 </li>
             @endif
 
-            {{-- Pagination Elements --}}
-            {{-- Always show first page --}}
+            {{-- First Page --}}
             <li class="page-item"><a class="page-link" href="{{ $paginator->url(1) }}">1</a></li>
-
-            {{-- Show ellipsis if current page - 2 is greater than or equal to 4 --}}
-            @if ($paginator->currentPage() - 2 >= 4)
+            @if ($paginator->currentPage() > 3)
                 <li class="page-item disabled" aria-disabled="true"><span class="page-link">...</span></li>
             @endif
 
-            {{-- Show page numbers within current page - 2 to current page + 2 range --}}
-            @for ($i = max(2, $paginator->currentPage() - 2); $i <= min($paginator->lastPage(), $paginator->currentPage() + 2); $i++)
-                @if ($i !== 1 && $i !== $paginator->lastPage())
-                    {{-- Avoid repeating first and last page --}}
-                    <li class="page-item @if ($i === $paginator->currentPage()) active @endif" aria-current="page">
-                        <a class="page-link" href="{{ $paginator->url($i) }}">{{ $i }}</a>
-                    </li>
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page != 1 && $page != $paginator->lastPage())
+                            @if ($page >= $paginator->currentPage() - 1 && $page <= $paginator->currentPage() + 1)
+                                <li class="page-item @if ($page == $paginator->currentPage()) active @endif"
+                                    aria-current="page"><a class="page-link"
+                                        href="{{ $url }}">{{ $page }}</a></li>
+                            @endif
+                        @endif
+                    @endforeach
                 @endif
-            @endfor
+            @endforeach
 
-            {{-- Show page 2 if current page - 2 is 3 --}}
-            @if ($paginator->currentPage() - 2 === 3)
-                <li class="page-item"><a class="page-link" href="{{ $paginator->url(2) }}">2</a></li>
-            @endif
-
-            {{-- Show ellipsis if current page + 2 is less than or equal to last page - 3 --}}
-            @if ($paginator->currentPage() + 2 < $paginator->lastPage() - 3)
+            {{-- Last Page --}}
+            @if ($paginator->currentPage() < $paginator->lastPage() - 2)
                 <li class="page-item disabled" aria-disabled="true"><span class="page-link">...</span></li>
             @endif
-
-            {{-- Show last page if current page + 2 is last page - 2 --}}
-            @if ($paginator->currentPage() + 2 === $paginator->lastPage() - 2)
+            @if ($paginator->currentPage() != $paginator->lastPage())
                 <li class="page-item"><a class="page-link"
-                        href="{{ $paginator->url($paginator->lastPage() - 1) }}">{{ $paginator->lastPage() - 1 }}</a>
-                </li>
+                        href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a></li>
             @endif
-
-            {{-- Always show last page --}}
-            <li class="page-item"><a class="page-link"
-                    href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a></li>
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
