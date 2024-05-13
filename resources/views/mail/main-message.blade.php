0414a068777e9abecd93f277b9ae1c268c5c65a8
miyu suzuki <bu.m6s.pq@gmail.com>
2024-05-14 08:50:57 +0900

    メール本文修正

--- resources/views/mail/main-message.blade.php
+++ resources/views/mail/main-message.blade.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Reseからのお知らせ</title>
+    <title>coachtechフリマからのお知らせ</title>
 </head>
 
 <body>
     <p class="name">{{ $name }}さん</p><br>
-    <p class="fixed-phrase">いつも「Rese」をご利用いただきまして誠にありがとうございます。</p>
+    <p class="fixed-phrase">いつも「coachtechフリマ」をご利用いただきまして誠にありがとうございます。</p>
     <p>{!! $main !!}</p>
 
 </body>
