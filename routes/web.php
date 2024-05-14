8f87c1c3fe3e78eb088c56c6c2933a95edd179cd
miyu suzuki <bu.m6s.pq@gmail.com>
2024-05-15 02:15:04 +0900

    不要コード削除

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\PurchaseController;
